2d1aed930c2be0c57c5f6051cc8850eda16a0043
ngc2392 <phillicon4733@gmail.com>
2019-02-02 17:17:40 -0500

    branch for assignment 4

---
+++ Assignment4/TestClass.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace Assignment4
+{
+    class TestClass
+    {
+        static void Main(string[] args)
+        {
+            Fraction f1 = new Fraction(1,3);
+            Console.WriteLine(f1.ToString());
+        }
+    }
+}
