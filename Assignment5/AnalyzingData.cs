c8ffc0ad92b71da4cec413c077e3c64d97fc82f4
ngc2392 <phillicon4733@gmail.com>
2019-03-07 18:11:27 -0500

    Analyzing data stuff

---
+++ Assignment5/AnalyzingData.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Assignment5
+{
+    class AnalyzingData
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        public static int minimumEntry() 
+        {
+
+        }
+
+        public static int maximumEntry()
+        {
+
+        }
+
+        public static int median()
+        {
+
+        }
+
+        public static int average() 
+        {
+
+        }
+
+        public static int standardDeviation() 
+        {
+
+        }
+
+    }
+}
