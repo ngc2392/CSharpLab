38fddb1a5e8e1d28cd774f1287fca4aebc0d3117
ngc2392 <phillicon4733@gmail.com>
2019-03-07 18:57:12 -0500

    min entry, max entry

--- Assignment5/AnalyzingData.cs
+++ Assignment5/AnalyzingData.cs
@@ -4,27 +4,53 @@ namespace Assignment5
 {
     class AnalyzingData
     {
+
+        public delegate void ArrayAnalysisDelegate(int[] numbers);
+
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+            int[] numbers = new int[] {};
         }
 
-        public static int minimumEntry() 
+        public static int minimumEntry(int[] numbers) 
+        {
+            int min = numbers[0];
+
+            foreach (int number in numbers)
+            {
+                if(number < min) 
+                {
+                    min = number;
+                }
+            }
+
+            return min;
+
+        }
+
+        public static int maximumEntry(int[] numbers)
+        {
+            int max = numbers[0];
+
+            foreach (int number in numbers)
+            {
+                if(number > max) 
+                {
+                    max = number;
+                }
+            }
+
+            return max;
+        }
+
+        public static int medianValue()
         {
 
         }
 
-        public static int maximumEntry()
-        {
-
-        }
-
-        public static int median()
-        {
-
-        }
-
-        public static int average() 
+        public static int averageValue() 
         {
 
         }
