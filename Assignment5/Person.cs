95185ecf05fc97c92684b8f6ec902604fca44121
logan <phillicon4733@gmail.com>
2019-04-11 15:05:59 -0400

    Person class

--- Assignment5/Person.cs
+++ Assignment5/Person.cs
@@ -1,40 +1,99 @@
 using System;
+using System.Collections.Generic;
 
 namespace Assignment5
 {
     class Person
     {
+        public delegate bool FilterDelegate(Person p);
         string name;
         int age;
 
-        public Person(String name, int age)
-        {
-            this.name = name;
-            this.age = age;
-        }
 
         public string Name 
         {
-            get{
+            
+            get
+            {
                 return this.name;
             }
-            set
+            private set
             {
                 this.name = value;
             }
         }
 
-        public int Age { get; set; }
+        public int Age { get; private set; }
+        public Person(string name, int age)
+        {
+            Name = name;
+            Age = age;
+        }
     
         static void Main(string[] args)
         {
+            Person p = new Person {
+                Name = "Bob",
+                Age = 33
+            };
+
+            Person p2 = new Person("bob", 15);
+            
+            Person[] people = new Person[] {p, p2};
+
+
+            FilterDelegate isChildDelegate = new FilterDelegate(isChildDelegate);
+
+            DisplayPeople(people, isChildDelegate));
+
            
         }
 
-        static void DisplayPeople()
+        public List<Person> DisplayPeople(Person[] people, FilterDelegate method)
         {
-            
+            List<Person> filteredList = new List<Person>();
+
+            foreach(var p in people)
+            {
+                if(method(p) == true)
+                    filteredList.Add(p);
+
+            }
+            return filteredList;
         }
+
+        public bool isChild(Person p)
+        {
+            if(p.Age < 18)
+                return true;
+            else
+                return false;
+        }
+
+        public bool firstNameOnly(Person p)
+        {
+            if(p.Name.Contains(" "))
+                return false;
+            else
+                return true;
+        }
+
+        public bool firstNameIsPalindrome(Person p)
+        {
+            string firstName = p.Name;
+
+            for (int i = 0; i < (firstName.Length) / 2; i++)
+            {
+                if(firstName[i] != firstName[firstName.Length - i - 1])
+                    return false;
+            }
+            return true;
+        }
+
+
+    
+
+        
 
     }
 }
