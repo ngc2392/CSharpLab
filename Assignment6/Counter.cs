196ea9f4f410ccc022ec41d365b467445c304c99
logan <phillicon4733@gmail.com>
2019-04-20 14:39:27 -0400

    initial files for Assignment 6

---
+++ Assignment6/Counter.cs
@@ -0,0 +1,22 @@
+/*
+
+Assignment 6a: Create a class named Counter that has an event named ThresholdReached. 
+ This event should be raised when a counter value equals or exceeds a threshold value.
+   The event should have two arguments - the threshold value and the date/time the 
+   threshold was reached.  You will not need to create a class or instance variables
+    for the event - look into how you can package arguments into an event using
+     the EventHandler<TEventArgs> (Links to an external site.)Links to an external site.
+      delegate.  Write a Main function to test your Counter class and the event handling.
+
+ */
+
+ namespace Assignment6 
+ {
+     class Counter{
+
+         static void Main(string[] args)
+         {
+             
+         }
+     }
+ }
