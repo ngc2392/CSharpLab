a74540ab2162bfea84810cbe8b7d1bc8da9d84e0
logan <phillicon4733@gmail.com>
2019-04-22 21:48:48 -0400

    changes

--- Assignment6/Counter.cs
+++ Assignment6/Counter.cs
@@ -16,19 +16,31 @@ namespace Assignment6
 {
 
 
+    // this class subscribes to the event 
     class Program
     {
         static void Main(string[] args)
         {
             Counter c = new Counter(new Random().Next(10));
+            // subscribe to the event 
+            c.ThresholdReached += HandleCustomEvent;
             Console.WriteLine("Hello World!");
+
+            Console.WriteLine("Press 'a' key to increase total");
+            while(Console.ReadKey(true).KeyChar == 'a')
+            {
+                Console.WriteLine("adding one");
+                c.Add(1);
+            }
+
         }
 
-        // This event should be raised when a counter value equals or exceeds a threshold value 
-        void HandleCustomEvent(object sender, ThresholdReachedEventArgs e)
+        // Define what actions to take when the event is raised
+        static void HandleCustomEvent(object sender, Counter.ThresholdReachedEventArgs e)
         {
             // Do something useful here
             Console.WriteLine("The threshold of {0} was reached at {1}.", e.Threshold, e.TimeReached);
+            Environment.Exit(0);
         }
 
     }
@@ -55,6 +67,7 @@ namespace Assignment6
                 ThresholdReachedEventArgs env = new ThresholdReachedEventArgs();
                 env.Threshold = threshold;
                 env.TimeReached = DateTime.Now;
+                // raise the event 
                 OnThresholdReached(env);
 
 
@@ -63,10 +76,14 @@ namespace Assignment6
 
         protected virtual void OnThresholdReached(ThresholdReachedEventArgs e)
         {
+            // Make a temporary copy of the event to avoid possibility of a race condition
             EventHandler<ThresholdReachedEventArgs> handler = ThresholdReached;
             if (handler != null)
             {
+                Console.WriteLine("Before handler");
                 handler(this, e);
+                Console.WriteLine("After handler");
+
             }
         }
 
