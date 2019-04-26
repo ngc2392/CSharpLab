095cbd3bdd0a83d7bbe36b40972ba1d4fd09e34c
logan <phillicon4733@gmail.com>
2019-04-26 19:04:16 -0400

    changes

--- Assignment7/Program.cs
+++ Assignment7/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.ComponentModel;
+using System.Drawing;
+using System.Windows.Forms;
 
 namespace Assignment7
 {
@@ -7,6 +10,22 @@ namespace Assignment7
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+        }
+    }
+
+    public class Form1 : Form
+    {
+        public Form1()
+        {
+
+        }
+
+
+        [STAThread]
+        public static void Main()
+        {
+            ApplicationException.EnableVisualStyles();
+            Application.Run(new Form1());
         }
     }
 }
