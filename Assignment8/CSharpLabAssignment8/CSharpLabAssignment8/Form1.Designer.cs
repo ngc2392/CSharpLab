66c98e10b59987832ee8f52dd5fe31adfd5c01b9
Phillips <lmp004@lvc.edu>
2019-04-30 17:08:20 -0400

    UI changes

--- Assignment8/CSharpLabAssignment8/CSharpLabAssignment8/Form1.Designer.cs
+++ Assignment8/CSharpLabAssignment8/CSharpLabAssignment8/Form1.Designer.cs
@@ -29,11 +29,239 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.userInput = new System.Windows.Forms.TextBox();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.nameOutputBox = new System.Windows.Forms.TextBox();
+            this.heightOutputBox = new System.Windows.Forms.TextBox();
+            this.massOutputBox = new System.Windows.Forms.TextBox();
+            this.hairColorOutputBox = new System.Windows.Forms.TextBox();
+            this.skinColorOutputBox = new System.Windows.Forms.TextBox();
+            this.eyeColorOutputBox = new System.Windows.Forms.TextBox();
+            this.birthYearOutputBox = new System.Windows.Forms.TextBox();
+            this.genderOutputBox = new System.Windows.Forms.TextBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.homePlanetOutputBox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // userInput
+            // 
+            this.userInput.Location = new System.Drawing.Point(25, 25);
+            this.userInput.Name = "userInput";
+            this.userInput.Size = new System.Drawing.Size(202, 20);
+            this.userInput.TabIndex = 0;
+            this.userInput.Text = "Enter a name";
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(22, 64);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Name";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(22, 115);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(38, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Height";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(25, 162);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(32, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Mass";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(173, 115);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(54, 13);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Skin color";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(174, 64);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(53, 13);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "Hair Color";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(177, 161);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(52, 13);
+            this.label6.TabIndex = 7;
+            this.label6.Text = "Eye Color";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(337, 64);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(53, 13);
+            this.label7.TabIndex = 8;
+            this.label7.Text = "Birth Year";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(337, 115);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(42, 13);
+            this.label8.TabIndex = 9;
+            this.label8.Text = "Gender";
+            // 
+            // nameOutputBox
+            // 
+            this.nameOutputBox.Location = new System.Drawing.Point(25, 80);
+            this.nameOutputBox.Name = "nameOutputBox";
+            this.nameOutputBox.Size = new System.Drawing.Size(100, 20);
+            this.nameOutputBox.TabIndex = 10;
+            // 
+            // heightOutputBox
+            // 
+            this.heightOutputBox.Location = new System.Drawing.Point(25, 131);
+            this.heightOutputBox.Name = "heightOutputBox";
+            this.heightOutputBox.Size = new System.Drawing.Size(100, 20);
+            this.heightOutputBox.TabIndex = 11;
+            // 
+            // massOutputBox
+            // 
+            this.massOutputBox.Location = new System.Drawing.Point(25, 178);
+            this.massOutputBox.Name = "massOutputBox";
+            this.massOutputBox.Size = new System.Drawing.Size(100, 20);
+            this.massOutputBox.TabIndex = 12;
+            // 
+            // hairColorOutputBox
+            // 
+            this.hairColorOutputBox.Location = new System.Drawing.Point(177, 80);
+            this.hairColorOutputBox.Name = "hairColorOutputBox";
+            this.hairColorOutputBox.Size = new System.Drawing.Size(100, 20);
+            this.hairColorOutputBox.TabIndex = 13;
+            // 
+            // skinColorOutputBox
+            // 
+            this.skinColorOutputBox.Location = new System.Drawing.Point(176, 131);
+            this.skinColorOutputBox.Name = "skinColorOutputBox";
+            this.skinColorOutputBox.Size = new System.Drawing.Size(100, 20);
+            this.skinColorOutputBox.TabIndex = 14;
+            // 
+            // eyeColorOutputBox
+            // 
+            this.eyeColorOutputBox.Location = new System.Drawing.Point(176, 178);
+            this.eyeColorOutputBox.Name = "eyeColorOutputBox";
+            this.eyeColorOutputBox.Size = new System.Drawing.Size(100, 20);
+            this.eyeColorOutputBox.TabIndex = 15;
+            // 
+            // birthYearOutputBox
+            // 
+            this.birthYearOutputBox.Location = new System.Drawing.Point(331, 80);
+            this.birthYearOutputBox.Name = "birthYearOutputBox";
+            this.birthYearOutputBox.Size = new System.Drawing.Size(100, 20);
+            this.birthYearOutputBox.TabIndex = 16;
+            // 
+            // genderOutputBox
+            // 
+            this.genderOutputBox.Location = new System.Drawing.Point(331, 131);
+            this.genderOutputBox.Name = "genderOutputBox";
+            this.genderOutputBox.Size = new System.Drawing.Size(100, 20);
+            this.genderOutputBox.TabIndex = 17;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(337, 162);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(68, 13);
+            this.label9.TabIndex = 18;
+            this.label9.Text = "Home Planet";
+            // 
+            // homePlanetOutputBox
+            // 
+            this.homePlanetOutputBox.Location = new System.Drawing.Point(331, 178);
+            this.homePlanetOutputBox.Name = "homePlanetOutputBox";
+            this.homePlanetOutputBox.Size = new System.Drawing.Size(100, 20);
+            this.homePlanetOutputBox.TabIndex = 19;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(576, 415);
+            this.Controls.Add(this.homePlanetOutputBox);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.genderOutputBox);
+            this.Controls.Add(this.birthYearOutputBox);
+            this.Controls.Add(this.eyeColorOutputBox);
+            this.Controls.Add(this.skinColorOutputBox);
+            this.Controls.Add(this.hairColorOutputBox);
+            this.Controls.Add(this.massOutputBox);
+            this.Controls.Add(this.heightOutputBox);
+            this.Controls.Add(this.nameOutputBox);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.userInput);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox userInput;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox nameOutputBox;
+        private System.Windows.Forms.TextBox heightOutputBox;
+        private System.Windows.Forms.TextBox massOutputBox;
+        private System.Windows.Forms.TextBox hairColorOutputBox;
+        private System.Windows.Forms.TextBox skinColorOutputBox;
+        private System.Windows.Forms.TextBox eyeColorOutputBox;
+        private System.Windows.Forms.TextBox birthYearOutputBox;
+        private System.Windows.Forms.TextBox genderOutputBox;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.TextBox homePlanetOutputBox;
     }
 }
 
