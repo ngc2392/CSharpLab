2d7ad81489f8e6ba03e1b94a9705447eeb62e15f
Phillips <lmp004@lvc.edu>
2019-05-01 13:51:59 -0400

    Assignment 7 Part A

---
+++ Assignment7/Assignment7PartA/Assignment7PartA/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Assignment7PartA
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Button_Click(object sender, EventArgs e)
+        {
+            System.Windows.Forms.MessageBox.Show("Hello! The button was clicked!");
+        }
+    }
+}
