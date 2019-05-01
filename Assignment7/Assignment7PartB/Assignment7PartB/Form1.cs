5db6264b07f0b2070ecde56148b9be6225c416d5
Phillips <lmp004@lvc.edu>
2019-05-01 13:56:36 -0400

    Assignment 7 Part B finished

---
+++ Assignment7/Assignment7PartB/Assignment7PartB/Form1.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Assignment7PartB
+{
+    public partial class Form1 : Form
+    {
+
+        double firstNumber;
+        double secondNumber;
+        string operation;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void plusButton_Click(object sender, EventArgs e)
+        {
+
+            getUserInput();
+            double answer = firstNumber + secondNumber;
+            setAnswer(answer);
+        }
+
+        private void minusButton_Click(object sender, EventArgs e)
+        {
+            getUserInput();
+            double answer = firstNumber - secondNumber;
+            setAnswer(answer);
+        }
+
+        private void multiplicationButton_Click(object sender, EventArgs e)
+        {
+            getUserInput();
+            double answer = firstNumber * secondNumber;
+            setAnswer(answer);
+        }
+
+        private void divisionButton_Click(object sender, EventArgs e)
+        {
+            getUserInput();
+            double answer = firstNumber / secondNumber;
+            setAnswer(answer);
+        }
+
+        private void getUserInput()
+        {
+            firstNumber = Convert.ToDouble(userInput1.Text);
+            secondNumber = Convert.ToDouble(userInput2.Text);
+        }
+
+        private void setAnswer(double answer)
+        {
+            answerBox.Text = Convert.ToString(answer);
+        }
+
+    }
+}
